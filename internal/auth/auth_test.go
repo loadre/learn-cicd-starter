3f5cbe820a7f4e30de3e7fb8663d93ff1738b4b8
R Loader mkII <30044659+loadre@users.noreply.github.com>
2024-05-15 18:31:43 -0400

    fixed ci (i swear)

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestFail(t *testing.T) {
-    h := http.Header{}
-    h.Add("Authorization", "")
-    _, err := GetAPIKey(h)
-    if err != ErrNoAuthHeaderIncluded{
-        t.Fatalf("expected: %v, got: %v", ErrNoAuthHeaderIncluded, err)
-    }
+	h := http.Header{}
+	h.Add("Authorization", "")
+	_, err := GetAPIKey(h)
+	if err != ErrNoAuthHeaderIncluded {
+		t.Fatalf("expected: %v, got: %v", ErrNoAuthHeaderIncluded, err)
+	}
 }
